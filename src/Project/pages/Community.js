74900d2b5a67e66235ddd1e28f3be10376417fb8
Dongha <132756629+kdh0825kr@users.noreply.github.com>
2024-07-23 09:40:01 +0900

    [#82] Community.js 수정

--- src/Project/pages/Community.js
+++ src/Project/pages/Community.js
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import MyTabs from "../components/myTabs";
 import './Community.css';
 
-const Community = ({ communities }) => {
-    const { communityId } = useParams();
-    const [posts, setPosts] = useState([]);
-    const navigate = useNavigate();
+const Community = () => {
+  const { communityId } = useParams();
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get('http://localhost:5000/posts')
@@ -23,28 +23,28 @@ const Community = ({ communities }) => {
     navigate(`/`);
   }
 
-    return (
-      <div>
-          <nav className="topNav">
-            <li className="Logo" onClick={goHome}>
-                <img className="imgLogo" src={require('../img/MainLogo.png')} alt="Logo" />
+  return (
+    <div>
+      <nav className="topNav">
+        <li className="Logo" onClick={goHome}>
+          <img className="imgLogo" src={require('../img/MainLogo.png')} alt="Logo" />
+        </li>
+        <li>
+          <MyTabs />
+        </li>
+      </nav>
+        <div className="community">
+        <h2>게시판</h2>
+        <ul className="posts-list">
+          {posts.map(post => (
+            <li key={post.post_id}>
+              <Link to={`/community/${communityId}/post/${post.post_id}`}>{post.title}</Link>
+              <p>작성자: {post.user_id}</p>
+              <p>작성 시간: {new Date(post.created_at).toLocaleString()}</p>
             </li>
-            <li>
-                <MyTabs />
-            </li>
-          </nav>
-      <div className="community">
-          <h2>{community.name}</h2>
-          <ul className="posts-list">
-              {posts.map(post => (
-                  <li key={post.post_id}>
-                      <Link to={`/community/${communityId}/post/${post.post_id}`}>{post.title}</Link>
-                      <p>작성자: {post.user_id}</p>
-                      <p>작성 시간: {new Date(post.created_at).toLocaleString()}</p>
-                  </li>
-              ))}
-          </ul>
-          <button className="new-post-button" onClick={handleNewPost}>글쓰기</button>
+          ))}
+        </ul>
+        <button className="new-post-button" onClick={handleNewPost}>글쓰기</button>
       </div>
     </div>
   );
